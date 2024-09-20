d126c8e30201efa148efa4f0355e5085c25f4b87
timaroblox5 <timaroblox9@gmail.com>
2024-09-20 21:14:07 +0300

    Первый коммит

---
+++ app/commands/page.tsx
@@ -0,0 +1,18 @@
+'use client'; // Указываем, что это клиентский компонент
+
+export default function commands() {
+
+  return (
+    <div>
+        <header>
+          <button id="test">Тест</button>
+          <button id="docs">ДОКУМЕНТАЦИЯ</button>
+          <button id="commands">КОМАНДЫ</button>
+          <button id="stats">СТАТИСТИКА</button>
+          <button id="support">ПОДДЕРЖИТЕ НАС</button>
+          <button id="otherfunction">⋮</button>
+          <button id="login">ВОЙТИ</button>
+        </header>
+    </div>
+  );
+}
